1114b681cf7fb8cbd7b2373cd0cb5e890831a997
ditinagrawal <ditin.agrawal05@gmail.com>
2025-09-07 14:42:49 +0530

    01: customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
+import { Button } from "@/components/ui/button"
+import { Checkbox } from "@/components/ui/checkbox"
+import { Input } from "@/components/ui/input"
+import { Progress } from "@/components/ui/progress"
+import { Textarea } from "@/components/ui/textarea"
+
 export default function Home() {
   return (
-    <div>
-      <p className="text-rose-400">Hello World</p>
+    <div className="p-4">
+      <div className="flex flex-col gap-y-4">
+        <div>
+          <Button variant="elevated">Click me</Button>
+        </div>
+        <div>
+          <Input placeholder="Enter your email" />
+        </div>
+        <div>
+          <Textarea placeholder="Enter your message" />
+        </div>
+        <div>
+          <Progress value={50} />
+        </div>
+        <div>
+          <Checkbox />
+        </div>
+      </div>
     </div>
   )
 }
